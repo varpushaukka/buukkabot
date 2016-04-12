83e8e5c8c1f910190af944d8bcc0724f82621ebd
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2016-04-12 20:28:04 +0300

    naming and timing

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from bottoken import token
 import requests
 import json
 import sqlite3
+import time
 
 class Bot:
 
@@ -11,22 +12,26 @@ class Bot:
         self.cur = self.conn.cursor()
 
     def messagelist(self):
-        r = requests.get('https://api.telegram.org/bot' + token + '/getUpdates')
-        messages = r.content
-        jsonmessages = json.loads(messages.decode('utf-8'))
-        msglist = jsonmessages['result']
-        parsedlist = []
-        for x in msglist:
-            parsedlist.append(self.parsemessage(x))
-        return(parsedlist)
+        while True:
+            r = requests.get('https://api.telegram.org/bot' + token + '/getUpdates')
+            messages = r.content
+            jsonmessages = json.loads(messages.decode('utf-8'))
+            msglist = jsonmessages['result']
+            parsedlist = []
+            for x in msglist:
+                parsedlist.append(self.parsemessage(x))
+            time.sleep(3)
+            return(parsedlist)
 
     def parsemessage(self, m):
-        mgrouptype = m['message']['chat']['type']
+        mgroup = m['message']['chat']['type']
         mdate = m['message']['date']
         msender = m['message']['from']['first_name'] + " " + m['message']['from']['last_name']
         mtext = m['message']['text']
         mmsgid = m['message']['message_id']
-        return dict(msgid=mmsgid, date=mdate, sender=msender, text = mtext, grouptype = mgrouptype)
+#        self.cur.execute('INSERT into messages VALUES (%i, %s, %i, %s, %s)', (mmsgid, mgroup, mdate, msender, mtext))
+#        self.conn.commit()
+        return dict(msgid=mmsgid, date=mdate, sender=msender, text = mtext, group = mgroup)
         
 
 if __name__ == '__main__':
--- testbot.py
+++ testbot.py
@@ -4,14 +4,14 @@ import bot
 class Testbot(unittest.TestCase):
     tbot = bot.Bot('logs')
 
-    #datastructure = [{sender: x, text: y, type: z, date: d, id: i}, {..}] 
+    #datastructure = [{sender: x, text: y, group: z, date: d, id: i}, {..}] 
     def test_messagelist(self):
         ml = self.tbot.messagelist()
         self.assertIsInstance(ml, list)
         all(self.assertIsInstance(x, dict) for x in ml)
         all(self.assertIn('sender', x) for x in ml)
         all(self.assertIn('text', x) for x in ml)
-        all(self.assertIn('grouptype', x) for x in ml)
+        all(self.assertIn('group', x) for x in ml)
         all(self.assertIn('date', x) for x in ml)
         all(self.assertIn('msgid', x) for x in ml)
 
