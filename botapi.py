2a71b33e8d267d157db2781108f7c95f340250a1
Henna Ruohonen <henna.ruohonen@helsinki.fi>
2016-04-14 07:58:58 +0300

    previous idea was bad

--- botapi.py
+++ botapi.py
@@ -7,22 +7,10 @@ class BotApi:
         self.conn = sqlite3.connect(dbname)
         self.cur = self.conn.cursor()
 
-    @route('/searchbyuser/<user>')    
-    def search_by_user(user):
-        return 'foo'
+    @route('/searchlogs')
+    def search_from_log_data(self, parameters)
+        return 'foo'    
     
-    @route('/searchbydate/<date>')
-    def search_by_date(date):
-        return 'foo'
-
-    @route('/searchbygroup/<group>')
-    def search_by_group(group):
-        return 'foo'
-
-    @route('searchbyword/<word>')
-    def search_by_word(word):
-        return 'foo'
-
 if __name__ == '__main__':
     a = BotApi('logs')
     run(host='localhost', port=8080)
